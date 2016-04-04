ed1f0c4a10a540d6bda496314191c2e014a60be6
Thomas Loesch <tommy.loesch@gmail.com>
2016-04-04 19:09:03 -0400

    Added button functionality

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -112,6 +112,7 @@
             this.btnSubmit.TabIndex = 11;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // lstBxOutput
             // 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -15,6 +16,38 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            StreamWriter outputFile = new StreamWriter("data.txt");
+
+            Double pay = CalcPay(txtBxHours.Text, txtBxRate.Text);
+
+            String outputString = txtBxName.Text + ", " + txtBxNumber.Text + ", " + pay;
+
+            outputFile.Write(outputString);
+
+            outputFile.Close();
+        }
+
+        private Double CalcPay(string _hours, string _rate)
+        {
+            double hours = Double.Parse(_hours),
+                   rate = Double.Parse(_rate),
+                   retval;
+
+            if(hours < 40)
+            {
+                retval = hours * rate;
+            }
+            else
+            {
+                retval = 40 * rate;
+                retval = retval + (hours - 40) * rate;
+            }
+
+            return retval;
         }
     }
 }
